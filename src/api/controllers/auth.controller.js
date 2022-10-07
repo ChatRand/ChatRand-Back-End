bc5798ecb16daf2dd91027be1114424cde90d801
Mikias Yonas <mikiasyonas512@gmail.com>
2022-10-07 13:13:38 +0300

    removed unnecessary console log

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -87,7 +87,6 @@ const userSignIn = async (
     });
   }
 
-  console.log(userData.password, user.password);
   const correctPassword = await compareHash(userData.password, user.password);
 
   if (!correctPassword) {
